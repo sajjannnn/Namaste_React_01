b30fcde5657ce71c3ff62a2ba0c02c670abc35a8
Sajjan Kumar <sajjanaasht@gmail.com>
2025-11-07 15:49:37 +0530

    learned about functional component and JSX

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
- const heading = React.createElement(
-    "h1", 
-    { id : "heading"},  // this contains attributes like id , classname
-    "Hello World from React!"
-);
+ import React from "react";
+ import ReactDOM from "react-dom/client";
+ 
 
-//heading is js object , not a h1 element
+ const Func = () => {
+   return <h2> this is functional component</h2>
+ }
+
+const JSXheading = <h1 id="heading">Hello World from React using JSX!</h1>;
 
 
+const Funt2 = () =>{
+  return <div>
+        <Func />
+        {JSXheading}
+        <h3>This is the function composition</h3>
+    </div>
+}
+
+
+Funt2();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<Funt2 />);
